1c1d9f1c6113ac1386f75f4cae553f2dcc8e79c0
prmdyazz <96212400+pramudyaazziz@users.noreply.github.com>
2023-09-01 10:53:00 +0700

    setup title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView, useRouter } from 'vue-router'
+
+
+const router = useRouter();
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
 </script>
 
 <template>
